d6aa455991d30e2b2451311bcc78cc5ad3fdec96
kangkigs <dlflq11@gmail.com>
2025-09-29 11:48:47 +0900

    Init

---
+++ src/test/java/com/sparta/vroomvroom/VroomVroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.vroomvroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VroomVroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

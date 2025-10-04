c049495e1116a7c1c950b5c3054e3dd8e4899453
kangkigs <dlflq11@gmail.com>
2025-10-04 10:50:45 +0900

    [feat] 회원가입 기능 Controller 엔드포인트 개발 (#7)

--- src/main/java/com/sparta/vroomvroom/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/vroomvroom/domain/user/controller/UserController.java
@@ -1,13 +1,22 @@
 package com.sparta.vroomvroom.domain.user.controller;
 
+import com.sparta.vroomvroom.domain.user.model.dto.request.UserSignupRequest;
 import com.sparta.vroomvroom.domain.user.service.UserService;
+import com.sparta.vroomvroom.global.conmon.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @PostMapping("/users/signup")
+    public BaseResponse signup(
+            @RequestBody UserSignupRequest userSignupRequest
+            ) {
+        userService.signup();
+        return new BaseResponse();
+    }
 }

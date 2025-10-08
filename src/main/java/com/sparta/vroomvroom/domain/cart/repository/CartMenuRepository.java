707bf2d926b0ef5e1d4e58c4842af47ee7a4ab22
Hyunho Kang <hhwj2270@gmail.com>
2025-10-08 15:02:42 +0900

    [feat] CartRepository, CartMenuRepository 쿼리 메서드 추가 (#5)

--- src/main/java/com/sparta/vroomvroom/domain/cart/repository/CartMenuRepository.java
+++ src/main/java/com/sparta/vroomvroom/domain/cart/repository/CartMenuRepository.java
@@ -1,10 +1,17 @@
 package com.sparta.vroomvroom.domain.cart.repository;
 
-import com.sparta.vroomvroom.domain.cart.model.entity.Cart;
 import com.sparta.vroomvroom.domain.cart.model.entity.CartMenu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CartMenuRepository extends JpaRepository<CartMenu, UUID> {
+
+    List<CartMenu> findByCart_CartId(UUID cartId);
+
+    Optional<CartMenu> findByCart_CartIdAndMenu_MenuId(UUID cartId, UUID menuId);
+
+    boolean existsByCart_CartId(UUID cartId);
 }

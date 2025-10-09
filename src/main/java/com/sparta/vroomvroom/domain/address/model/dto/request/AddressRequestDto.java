46af5ee3d4e7983d254782ef7b41b955ea1dd42f
jjaenie <wogus990519@gmail.com>
2025-10-10 01:54:48 +0900

    [feat] AddressRequestDto 클래스명 오타 수정 (#20)

--- src/main/java/com/sparta/vroomvroom/domain/address/model/dto/request/AddressRequestDto.java
+++ src/main/java/com/sparta/vroomvroom/domain/address/model/dto/request/AddressRequestDto.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 
 
 @Getter
-public class AddressReqeustDto {
+public class AddressRequestDto {
     private String addressName;
     private String address;
     private String detailAddress;

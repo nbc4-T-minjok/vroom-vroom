c30bf05a1656c2b64ce5e229be28ac29b6e65b2b
jjaenie <wogus990519@gmail.com>
2025-10-10 01:58:14 +0900

    [feat] 서비스로직 수정 - 배송지 수정 중 location 업데이트 추가, 중복 코드 메서드로 추 (#20)

--- src/main/java/com/sparta/vroomvroom/domain/address/controller/AddressController.java
+++ src/main/java/com/sparta/vroomvroom/domain/address/controller/AddressController.java
@@ -1,9 +1,8 @@
 package com.sparta.vroomvroom.domain.address.controller;
 
-import com.sparta.vroomvroom.domain.address.model.dto.request.AddressReqeustDto;
+import com.sparta.vroomvroom.domain.address.model.dto.request.AddressRequestDto;
 import com.sparta.vroomvroom.domain.address.model.dto.response.AddressResponseDto;
 import com.sparta.vroomvroom.domain.address.service.AddressService;
-import com.sparta.vroomvroom.domain.user.model.entity.User;
 import com.sparta.vroomvroom.domain.user.repository.UserRepository;
 import com.sparta.vroomvroom.global.conmon.BaseResponse;
 import com.sparta.vroomvroom.global.security.UserDetailsImpl;
@@ -11,10 +10,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.Type;
 import java.util.List;
 import java.util.UUID;
 
@@ -29,7 +26,7 @@ public class AddressController {
 
     // 배송지 등록
     @PostMapping("addresses")
-    public BaseResponse createAddress(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody AddressReqeustDto reqeustDto) {
+    public BaseResponse createAddress(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody AddressRequestDto reqeustDto) {
         Long userId = userDetails.getUser().getUserId();
 //        System.out.println("//// userId : " + userId);
         addressService.createAddress(userId, reqeustDto);
@@ -47,9 +44,9 @@ public class AddressController {
     // 배송지 정보 수정
     @Secured({"ROLE_MANAGER","ROLE_CUSTOMER"})
     @PatchMapping("addresses/{userAddressId}")
-    public BaseResponse patchAddress(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable UUID userAddressId, @RequestBody AddressReqeustDto reqeustDto) {
+    public BaseResponse patchAddress(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable UUID userAddressId, @RequestBody AddressRequestDto requestDto) {
         Long userId = userDetails.getUser().getUserId();
-        addressService.patchAddress(userId, userAddressId, reqeustDto);
+        addressService.patchAddress(userId, userAddressId, requestDto);
         return new BaseResponse<>();
     }
 
--- src/main/java/com/sparta/vroomvroom/domain/address/service/AddressService.java
+++ src/main/java/com/sparta/vroomvroom/domain/address/service/AddressService.java
@@ -1,17 +1,14 @@
 package com.sparta.vroomvroom.domain.address.service;
 
-import com.sparta.vroomvroom.domain.address.model.dto.request.AddressReqeustDto;
+import com.sparta.vroomvroom.domain.address.model.dto.request.AddressRequestDto;
 import com.sparta.vroomvroom.domain.address.model.dto.response.AddressResponseDto;
 import com.sparta.vroomvroom.domain.address.model.entity.Address;
 import com.sparta.vroomvroom.domain.address.repository.AddressRepository;
 import com.sparta.vroomvroom.domain.user.model.entity.User;
 import com.sparta.vroomvroom.domain.user.repository.UserRepository;
-import com.sparta.vroomvroom.global.conmon.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.GeometryFactory;
-import org.locationtech.jts.geom.Point;
-import org.locationtech.jts.geom.PrecisionModel;
+import lombok.extern.slf4j.Slf4j;
+import org.locationtech.jts.geom.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,15 +19,16 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class AddressService {
     private final AddressRepository addressRepository;
     private final UserRepository userRepository;
 
     // 배송지 등록
-    public void createAddress(Long userId, AddressReqeustDto requestDto) {
+    public void createAddress(Long userId, AddressRequestDto requestDto) {
 
         // 사용자 검증
-        User user = userRepository.findById((userId)).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+        User user = validateUser(userId);
 
         // 기본배송지 중복 방지
         if (requestDto.isDefault()) {
@@ -39,11 +37,10 @@ public class AddressService {
 
         // 우편번호 검증
         String zipCode = requestDto.getZipCode();
-        if(!zipCode.matches("\\d{5}")) {
-           throw new IllegalArgumentException("우편번호가 올바르지 않습니다. 우편번호는 5자리 숫자여야 합니다.");
+        if (!zipCode.matches("\\d{5}")) {
+            throw new IllegalArgumentException("우편번호가 올바르지 않습니다. 우편번호는 5자리 숫자여야 합니다.");
         }
 
-        // 위치 정보 검증
         // location에서 lat, lng 꺼내기
         if (requestDto.getLocation() == null) {
             throw new IllegalArgumentException("location 필드는 필수입니다. (lat, lng 포함)");
@@ -52,20 +49,11 @@ public class AddressService {
         Double lat = requestDto.getLocation().getLat(); // 위도
         Double lng = requestDto.getLocation().getLng(); // 경도
 
-        if (lat == null || lng == null) {
-            throw new IllegalArgumentException("위도(lat)와 경도(lng)는 모두 필수입니다.");
-        }
-        if (lat < -90 || lat > 90) {
-            throw new IllegalArgumentException("위도(lat)는 -90 ~ 90 사이의 값이어야 합니다.");
-        }
-        if (lng < -180 || lng > 180) {
-            throw new IllegalArgumentException("경도(lng)는 -180 ~ 180 사이의 값이어야 합니다.");
-        }
+        // 위치 정보 검증
+        validateLocation(lat, lng);
 
         // 포인트 생성
-        GeometryFactory geomFactory = new GeometryFactory(new PrecisionModel(), 4326);
-        Coordinate coord = new Coordinate(lng, lat); // 경도, 위도 순서로 변경 필수
-        Point location = geomFactory.createPoint(coord);
+        Point location = createPoint(lat, lng);
 
         //엔티티 저장
         Address address = new Address(
@@ -83,13 +71,13 @@ public class AddressService {
     // 배송지 목록 조회
     public List<AddressResponseDto> getAlladdresses(Long userId) {
         // 사용자 검증
-        User user = userRepository.findById((userId)).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+        User user = validateUser(userId);
 
         // 배송지 목록 조회
         List<Address> addresses = addressRepository.findAllByUserId(userId);
 
         List<AddressResponseDto> responseDtos = new ArrayList<>();
-        for(Address a : addresses) {
+        for (Address a : addresses) {
             responseDtos.add(new AddressResponseDto(a));
         }
 
@@ -98,23 +86,31 @@ public class AddressService {
     }
 
     // 배송지 수정
-    public void patchAddress(Long userId, UUID userAddressId, AddressReqeustDto requestDto) {
+    public void patchAddress(Long userId, UUID userAddressId, AddressRequestDto requestDto) {
         // 사용자 검증
-        User user = userRepository.findById((userId)).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+        User user = validateUser(userId);
 
         // 배송지아이디 검증
-        Address address = addressRepository.findById(userAddressId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 배송지아이디입니다."));
-
-        // 배송지 소유자 검증 (로그인 한 사용자의 주소인지)
-        if (!address.getUser().getUserId().equals(userId)) {
-            throw new IllegalArgumentException("본인 소유의 배송지만 수정할 수 있습니다.");
-        }
+        Address address = validateAddressOwnership(userId, userAddressId);
 
         // 변경 내용 반영
         if(requestDto.getAddressName() != null) address.setAddressName(requestDto.getAddressName());
         if(requestDto.getAddress() != null) address.setAddress(requestDto.getAddress());
         if(requestDto.getDetailAddress() != null) address.setDetailAddress(requestDto.getDetailAddress());
         if(requestDto.getZipCode() != null) address.setZipCode(requestDto.getZipCode());
+        if(requestDto.getLocation() != null) {
+            // location에서 lat, lng 꺼내기
+            Double lat = requestDto.getLocation().getLat(); // 위도
+            Double lng = requestDto.getLocation().getLng(); // 경도
+
+            // 위치 정보 검증
+            validateLocation(lat, lng);
+
+            // 포인트 생성
+            Point location = createPoint(lat, lng);
+
+            address.setLocation(location);
+        }
         if (requestDto.isDefault()) {
             addressRepository.updateIsDefaultToFalse(userId);
             address.setDefault(true);
@@ -123,18 +119,14 @@ public class AddressService {
         addressRepository.save(address);
     }
 
+
     // 기본배송지 변경
     public void changeDefaultAddress(Long userId, UUID userAddressId) {
         // 사용자 검증
-        User user = userRepository.findById((userId)).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+        User user = validateUser(userId);
 
         // 배송지아이디 검증
-        Address address = addressRepository.findById(userAddressId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 배송지아이디입니다."));
-
-        // 배송지 소유자 검증 (로그인 한 사용자의 주소인지)
-        if (!address.getUser().getUserId().equals(userId)) {
-            throw new IllegalArgumentException("본인 소유의 배송지만 수정할 수 있습니다.");
-        }
+        Address address = validateAddressOwnership(userId, userAddressId);
 
         // 기존 기본배송지 제거
         addressRepository.updateIsDefaultToFalse(userId);
@@ -147,21 +139,55 @@ public class AddressService {
     // 배송지 삭제
     public void deleteAddress(Long userId, UUID userAddressId) {
         // 사용자 검증
-        User user = userRepository.findById((userId)).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+        User user = validateUser(userId);
 
         // 배송지아이디 검증
-        Address address = addressRepository.findById(userAddressId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 배송지아이디입니다."));
-
-        //  배송지 소유자 검증 (로그인 한 사용자의 주소인지)
-        if(!address.getUser().getUserId().equals(userId)) {
-            throw new IllegalArgumentException("본인 소유의 배송지만 삭제할 수 있습니다.");
-        }
+        Address address = validateAddressOwnership(userId, userAddressId);
 
         // 기본배송지가 not null이므로 삭제 불가 처리
-        if(address.isDefault()) {
+        if (address.isDefault()) {
             throw new IllegalArgumentException("기본배송지는 삭제할 수 없습니다.");
         }
 
         addressRepository.deleteById(userAddressId);
+    }
+
+    // 사용자 검증 메서드
+    private User validateUser(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+    }
+
+    // 배송지 소유 검증 메서드
+    private Address validateAddressOwnership(Long userId, UUID addressId) {
+        Address address = addressRepository.findById(addressId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 배송지입니다."));
+
+        if (!address.getUser().getUserId().equals(userId)) {
+            throw new IllegalArgumentException("본인 소유의 배송지만 접근할 수 있습니다.");
+        }
+        return address;
+    }
+
+    // 위치 정보 검증 메서드
+    private void validateLocation(Double lat, Double lng) {
+        if (lat == null || lng == null) {
+            throw new IllegalArgumentException("위도(lat)와 경도(lng)는 모두 필수입니다.");
+        }
+        if (lat < -90 || lat > 90) {
+            throw new IllegalArgumentException("위도(lat)는 -90 ~ 90 사이의 값이어야 합니다.");
+        }
+        if (lng < -180 || lng > 180) {
+            throw new IllegalArgumentException("경도(lng)는 -180 ~ 180 사이의 값이어야 합니다.");
+        }
+    }
+
+    // 포인트 생성 메서드
+    private Point createPoint(Double lat, Double lng) {
+        GeometryFactory geomFactory = new GeometryFactory(new PrecisionModel(), 4326);
+        Coordinate coord = new Coordinate(lng, lat); // 경도, 위도 순서로 변경 필수
+        Point location = geomFactory.createPoint(coord);
+        location.setSRID(4326); // ✅ SRID 명시
+        return location;
     }
 }

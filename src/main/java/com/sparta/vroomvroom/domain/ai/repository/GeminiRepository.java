ca7a12a6fb7531a184cce1be8ca6b1f5fbdbc17f
minsik0 <qwew1212@naver.com>
2025-10-11 02:35:08 +0900

    GeminiRepository  수정

--- src/main/java/com/sparta/vroomvroom/domain/ai/repository/GeminiRepository.java
+++ src/main/java/com/sparta/vroomvroom/domain/ai/repository/GeminiRepository.java
@@ -1,9 +1,8 @@
 package com.sparta.vroomvroom.domain.ai.repository;
-
-import com.sparta.vroomvroom.domain.ai.model.dto.request.GeminiRequest;
+import com.sparta.vroomvroom.domain.ai.model.entity.AiApiLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface GeminiRepository extends JpaRepository<GeminiRequest, UUID> {
+public interface GeminiRepository extends JpaRepository<AiApiLog, UUID> {
 }

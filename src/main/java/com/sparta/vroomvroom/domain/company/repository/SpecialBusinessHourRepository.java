b0f8c5c250ee44cbe1b24493f3ff99b31f1ea406
hwangtae <htk5205@naver.com>
2025-10-10 12:43:13 +0900

    [feat] 특별영업시간 쿼리 메서드 추가 (#26)

--- src/main/java/com/sparta/vroomvroom/domain/company/repository/SpecialBusinessHourRepository.java
+++ src/main/java/com/sparta/vroomvroom/domain/company/repository/SpecialBusinessHourRepository.java
@@ -3,7 +3,9 @@ package com.sparta.vroomvroom.domain.company.repository;
 import com.sparta.vroomvroom.domain.company.model.entity.SpecialBusinessHour;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SpecialBusinessHourRepository extends JpaRepository<SpecialBusinessHour, UUID> {
+    List<SpecialBusinessHour> findAllByCompanyId(UUID companyId);
 }

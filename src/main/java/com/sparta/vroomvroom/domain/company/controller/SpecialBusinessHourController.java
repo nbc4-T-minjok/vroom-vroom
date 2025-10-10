0f0e9e56c5a2863cd0c96cda7d9b233281eaa8e1
hwangtae <htk5205@naver.com>
2025-10-10 12:42:37 +0900

    [feat] 특별영업시간 컨트롤러 구현 (#26)

--- src/main/java/com/sparta/vroomvroom/domain/company/controller/SpecialBusinessHourController.java
+++ src/main/java/com/sparta/vroomvroom/domain/company/controller/SpecialBusinessHourController.java
@@ -1,11 +1,52 @@
 package com.sparta.vroomvroom.domain.company.controller;
 
+import com.sparta.vroomvroom.domain.company.model.dto.request.SpecialBusinessHourRequestDto;
+import com.sparta.vroomvroom.domain.company.model.dto.response.SpecialBusinessHourResponseDto;
+import com.sparta.vroomvroom.domain.company.service.SpecialBusinessHourService;
+import com.sparta.vroomvroom.global.conmon.BaseResponse;
+import com.sparta.vroomvroom.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Secured({"ROLE_OWNER", "ROLE_MANAGER"})
 public class SpecialBusinessHourController {
+    private final SpecialBusinessHourService specialBusinessHourService;
+
+    @PostMapping("/companies/{companyId}/special_business_hours")
+    public BaseResponse createSpecialBusinessHour(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                  @PathVariable UUID companyId,
+                                                  @RequestBody SpecialBusinessHourRequestDto requestDto) {
+        specialBusinessHourService.createSpecialBusinessHour(userDetails.getUser().getUserId(), companyId, requestDto);
+        return new BaseResponse();
+    }
+
+    @GetMapping("/companies/{companyId}/special_business_hours")
+    public BaseResponse getSpecialBusinessHours(@PathVariable UUID companyId) {
+        List<SpecialBusinessHourResponseDto> responseDtos = specialBusinessHourService.getSpecialBusinessHours(companyId);
+        return new BaseResponse(responseDtos);
+    }
+
+    @PatchMapping("/companies/{companyId}/special_business_hours/{specialBusinessHourId}")
+    public BaseResponse updateSpecialBusinessHour(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                   @PathVariable UUID companyId, @PathVariable UUID specialBusinessHourId,
+                                                   @RequestBody SpecialBusinessHourRequestDto requestDto) {
+        SpecialBusinessHourResponseDto responseDto = specialBusinessHourService.updateSpecialBusinessHour(userDetails.getUser().getUserId(), companyId, specialBusinessHourId, requestDto);
+        return new BaseResponse(responseDto);
+    }
+
+    @DeleteMapping("/companies/{companyId}/special_business_hours/{specialBusinessHourId}")
+    public BaseResponse deleteSpecialBusinessHour(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                  @PathVariable UUID companyId, @PathVariable UUID specialBusinessHourId){
+        specialBusinessHourService.deleteSpecialBusinessHour(userDetails.getUser().getUserId(), companyId, specialBusinessHourId);
+        return new BaseResponse<>();
+    }
 }
+

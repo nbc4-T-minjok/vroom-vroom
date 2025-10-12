1b4ee27745884ead742ff4100fa7b2456678fed0
jjaenie <wogus990519@gmail.com>
2025-10-12 23:27:40 +0900

    [feat] AddressController, BusinessHourController에 swagger annotation 적용 (#36)

--- src/main/java/com/sparta/vroomvroom/domain/company/controller/BusinessHourController.java
+++ src/main/java/com/sparta/vroomvroom/domain/company/controller/BusinessHourController.java
@@ -5,7 +5,12 @@ import com.sparta.vroomvroom.domain.company.model.dto.BusinessHourRequestDto;
 import com.sparta.vroomvroom.domain.company.model.dto.BusinessHourResponseDto;
 import com.sparta.vroomvroom.domain.company.service.BusinessHourService;
 import com.sparta.vroomvroom.global.conmon.BaseResponse;
+import com.sparta.vroomvroom.global.conmon.constants.SwaggerDescription;
+import com.sparta.vroomvroom.global.conmon.constants.SwaggerExamples;
 import com.sparta.vroomvroom.global.security.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -22,6 +27,15 @@ public class BusinessHourController {
     private final BusinessHourService businessHourService;
 
     // 영업시간 등록
+    @Operation(summary = "영업시간 등록 API", description = SwaggerDescription.BUSINESS_HOUR_CREATE_REQUEST,
+            requestBody =  @io.swagger.v3.oas.annotations.parameters.RequestBody (
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = {
+                                    @ExampleObject(value = SwaggerExamples.BUSINESS_HOUR_CREATE_REQUEST)
+                            }
+                    )
+            ))
     @Secured({"ROLE_MANAGER","ROLE_OWNER"})
     @PostMapping("/companies/{companyId}/business_hours")
     public BaseResponse createBusinessHour(@PathVariable UUID companyId, @RequestBody BusinessHourRequestDto requestDto) {
@@ -30,6 +44,7 @@ public class BusinessHourController {
     }
 
     // 영업시간 조회
+    @Operation(summary = "영업시간 조회 API")
     @Secured({"ROLE_MANAGER","ROLE_OWNER"})
     @GetMapping("/companies/{companyId}/business_hours")
     public BaseResponse getBusinessHour(@PathVariable UUID companyId) {
@@ -38,6 +53,15 @@ public class BusinessHourController {
     }
 
     // 영업시간 수정
+    @Operation(summary = "영업시간 수정 API", description = SwaggerDescription.BUSINESS_HOUR_UPDATE_REQUEST,
+            requestBody =  @io.swagger.v3.oas.annotations.parameters.RequestBody (
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = {
+                                    @ExampleObject(value = SwaggerExamples.BUSINESS_HOUR_UPDATE_REQUEST)
+                            }
+                    )
+            ))
     @Secured({"ROLE_MANAGER","ROLE_OWNER"})
     @PatchMapping("/companies/{companyId}/business_hours/{businessHourId}")
     public BaseResponse updateBusinessHour(@PathVariable UUID companyId, @PathVariable UUID businessHourId,
@@ -47,6 +71,7 @@ public class BusinessHourController {
     }
 
     // 영업시간 삭제
+    @Operation(summary = "영업시간 삭제 API")
     @Secured({"ROLE_MANAGER","ROLE_OWNER"})
     @DeleteMapping("/companies/{companyId}/business_hours/{businessHourId}")
     public BaseResponse deleteBusinessHour(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable UUID companyId, @PathVariable UUID businessHourId) {

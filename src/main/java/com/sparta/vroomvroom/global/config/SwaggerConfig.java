be669432fcbb3d043f09cb612ad5e8ffd3f0eb96
jjaenie <wogus990519@gmail.com>
2025-10-12 21:06:53 +0900

    [feat] swagger 의존성 수정, config 파일 작성 (#36)

---
+++ src/main/java/com/sparta/vroomvroom/global/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.sparta.vroomvroom.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("vroom-vroom API Test")
+                .description("[8조 : T의 민족] 음식 주문 관리 플랫폼 API Test")
+                .version("1.0.0");
+    }
+}

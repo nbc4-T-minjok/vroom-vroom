32caef53a274aece23ddb32e3ba5f8aef98043da
minsik0 <qwew1212@naver.com>
2025-10-13 23:12:48 +0900

    Merge branch 'dev' into feat/menu/swagger - resolve SwaggerExamples and SwaggerDescription

--- src/main/java/com/sparta/vroomvroom/global/conmon/swagger/SwaggerDescription.java
+++ src/main/java/com/sparta/vroomvroom/global/conmon/swagger/SwaggerDescription.java
@@ -24,6 +24,48 @@ public class SwaggerDescription {
             "영업시간 조회 후 반환된 businessHourId 값과 companyId 값이 필요합니다.<br>" +
             "요청 가능 권한 : customer / manager";
 
+    // 특별영업시간 등록 설명
+    public static final String SPECIAL_BUSINESS_HOUR_CREATE_REQUEST =
+            "먼저 회원가입과 업체 등록 진행 후 요청을 해주세요.<br>" +
+                    "업체 조회 후 반환된 companyId 값이 필요합니다.<br>" +
+                    "요청 가능 권한 : owner / manager";
+
+    // 특별영업시간 수정 및 삭제 설명
+    public static final String SPECIAL_BUSINESS_HOUR_UPDATE_REQUEST =
+            "먼저 회원가입과 업체 등록 진행 후 요청을 해주세요.<br>" +
+                    "특별영업시간 조회 후 반환된 companyId 값과 specialBusinessHourId 값이 필요합니다.<br>" +
+                    "요청 가능 권한 : owner / manager";;
+    // 업체 카테고리 등록 설명
+    public static final String COMPANY_CATEGORY_CREATE_REQUEST =
+            "관리자 권한만 요청을 해주세요.<br>" +
+                    "요청 가능 권한 : manager / master";
+
+    // 업체 카테고리 수정 및 삭제 설명
+    public static final String COMPANY_CATEGORY_UPDATE_REQUEST =
+            "관리자 권한만 요청을 해주세요.<br>" +
+                    "업체 카테고리 조회 후 반환된 companyCategoryId 값이 필요합니다.<br>"  +
+                    "요청 가능 권한 : manager / master";
+    // 주문 생성 설명
+    public static final String ORDER_CREATE_REQUEST =
+            "회원가입과 로그인 진행 후 주문 생성 요청을 해주세요.<br>" +
+            "companyId 값과 userAddressId 값이 필요합니다.<br>" +
+            "요청 가능 권한 : customer";
+    public static final String USER_SIGNUP_REQUEST = """
+            userName, email, phoneNumber는 중복이 불가능 합니다.
+            비밀번호는 대소문자,특수문자,숫자 모두를 포함해서 8~15자로 설정해주세요.
+            """;
+
+    //회원 정보 수정 예시
+    public static final String USER_DETAIL_UPDATE_REQUEST = """
+            userName, email, phoneNumber는 중복이 불가능 합니다.
+            변경하고 싶은 항목만 선택적으로 변경할 수 있습니다.
+            """;
+
+    //비밀번호 변경 예시
+    public static final String USER_PASSWORD_CHANGE_REQUEST = """
+            비밀번호는 대소문자,특수문자,숫자 모두를 포함해서 8~15자로 설정해주세요.
+            """;
+
     // 메뉴 등록 설명
     public static final String MENU_CREATE_REQUEST =
             """

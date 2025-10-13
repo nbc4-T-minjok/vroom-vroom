5b9d7f2cadc12ea0b44d841a1b17b075a350adc7
Hyunho Kang <hhwj2270@gmail.com>
2025-10-13 20:22:39 +0900

    [feat] OrderController 주문 상태 변경 API 추가 & swagger 적용

--- src/main/java/com/sparta/vroomvroom/domain/order/controller/OrderController.java
+++ src/main/java/com/sparta/vroomvroom/domain/order/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.sparta.vroomvroom.domain.order.controller;
 
 import com.sparta.vroomvroom.domain.order.model.dto.request.CancelOrderRequest;
 import com.sparta.vroomvroom.domain.order.model.dto.request.CreateOrderRequest;
+import com.sparta.vroomvroom.domain.order.model.dto.request.UpdateOrderStatusRequest;
 import com.sparta.vroomvroom.domain.order.model.dto.response.CompanyOrderListResponse;
 import com.sparta.vroomvroom.domain.order.model.dto.response.OrderDetailResponse;
 import com.sparta.vroomvroom.domain.order.model.dto.response.OrderListResponse;
@@ -46,19 +47,6 @@ public class OrderController {
         return new BaseResponse<>();
     }
 
-    @Operation(summary = "주문 상세 조회 API")
-    @GetMapping("/orders/{orderId}")
-    public BaseResponse<OrderDetailResponse> getOrderDetail(
-            @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @PathVariable UUID orderId
-    ) {
-        OrderDetailResponse response = orderService.getOrderDetail(
-                userDetails.getUser(),
-                orderId
-        );
-        return new BaseResponse<>(response);
-    }
-
     @Operation(summary = "고객용 주문 목록 조회 API")
     @GetMapping("/orders")
     public BaseResponse<OrderListResponse> getOrders(
@@ -74,17 +62,6 @@ public class OrderController {
                 orderStatus
         );
         return new BaseResponse<>(response);
-    }
-
-    @Operation(summary = "주문 취소 API")
-    @DeleteMapping("/orders/{orderId}")
-    public BaseResponse<Void> cancelOrder(
-            @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @PathVariable UUID orderId,
-            @Valid @RequestBody CancelOrderRequest request
-    ) {
-        orderService.cancelOrder(userDetails.getUser(), orderId, request);
-        return new BaseResponse<>();
     }
 
     @Operation(summary = "업체용 주문 목록 조회 API")
@@ -109,4 +86,51 @@ public class OrderController {
         );
         return new BaseResponse<>(response);
     }
+
+    @Operation(summary = "주문 상세 조회 API")
+    @GetMapping("/orders/{orderId}")
+    public BaseResponse<OrderDetailResponse> getOrderDetail(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @PathVariable UUID orderId
+    ) {
+        OrderDetailResponse response = orderService.getOrderDetail(
+                userDetails.getUser(),
+                orderId
+        );
+        return new BaseResponse<>(response);
+    }
+    @Operation(summary = "주문상태 수정 API", description = SwaggerDescription.ORDER_STATUS_UPDATE_REQUEST,
+            requestBody =  @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = {
+                                    @ExampleObject(value = SwaggerExamples.ORDER_STATUS_UPDATE_REQUEST)
+                            }
+                    )
+            ))
+
+    @PatchMapping("/orders/{orderId}/status")
+    public BaseResponse<Void> updateOrderStatus(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @PathVariable UUID orderId,
+            @Valid @RequestBody UpdateOrderStatusRequest request
+    ) {
+        orderService.updateOrderStatus(userDetails.getUser(), orderId, request);
+        return new BaseResponse<>();
+    }
+
+    @Operation(summary = "주문 취소 API")
+    @DeleteMapping("/orders/{orderId}")
+    public BaseResponse<Void> cancelOrder(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @PathVariable UUID orderId,
+            @Valid @RequestBody CancelOrderRequest request
+    ) {
+        orderService.cancelOrder(userDetails.getUser(), orderId, request);
+        return new BaseResponse<>();
+    }
+
+
+
+
 }

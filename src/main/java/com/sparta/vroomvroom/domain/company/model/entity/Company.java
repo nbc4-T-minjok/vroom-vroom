08fee73f76423d191c66d2eae657dbbb4fa26a59
kangkigs <dlflq11@gmail.com>
2025-10-13 11:45:59 +0900

    [fix] user-company 연관관계 수정 (#25)

--- src/main/java/com/sparta/vroomvroom/domain/company/model/entity/Company.java
+++ src/main/java/com/sparta/vroomvroom/domain/company/model/entity/Company.java
@@ -20,8 +20,8 @@ public class Company extends BaseEntity {
     @Column(name = "company_id")
     private UUID companyId;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
